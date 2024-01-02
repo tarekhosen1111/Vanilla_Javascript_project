e1872eb7af88c767eeebe906d32fc60a295bfdb1
tarekhosen1111 <tarekhosen1111@gmail.com>
2024-01-02 16:52:43 +0600

    simple

--- counter/app.js
+++ counter/app.js
@@ -22,7 +22,7 @@ btns.forEach(function (btn) {
       value.style.color = "red";
     }
     if (count === 0) {
-      value.style.color = "#222";
+      value.style.color ="white";
     }
     value.textContent = count;
   });
